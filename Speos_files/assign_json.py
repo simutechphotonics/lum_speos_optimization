303f94e5d5da15693d837297fd3b48ad32f30905
Stephen Lin <slin@simutechgroup.com>
2025-02-18 19:15:25 -0800

    update to connect to speos

--- Speos_files/assign_json.py
+++
@@ -1,8 +0,0 @@
-# Python Script, API Version = V251
-
-
-ListSOPIndex = 0
-SopPluginConfigurationPath = r".\SPEOS input files\p474_dc20_d400_sa45_wl624.json"
-material = SpeosSim.Material(Selection.Create(GetRootPart().GetChildren[ICustomObject]()[12]).Items[0])
-SurfaceLayer = material.ListSOP[ListSOPIndex]
-SurfaceLayer.SopPluginConfigurationPath = SopPluginConfigurationPath
